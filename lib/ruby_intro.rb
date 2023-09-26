720423d8e43184d3242f40f5f0605603568ade3f
Sarah Hart <sarah.hart.nc@gmail.com>
2023-09-25 20:25:05 -0400

    debug 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,19 +3,13 @@
 # Part 1
 
 def sum arr
-  array.sum
+  arr.sum
 end
 
 # Define a method max_2_sum(array) that returns the sum of the two largest elements.
 def max_2_sum arr
-  if array.empty?
-    return 0
-  elsif array.length == 1
-    return array[0]
-  else 
-    sorted_array = array.sort
-    return sorted_array[-1] + sorted_array[-2]
-  end
+  #sort the array in decending order, sum the first 2. 
+  arr.sort.reverse.take(2).sum
 end
 
 # Define a method sum_to_n?(array, n) that returns true if any two elements in the array sum to n.
@@ -53,5 +47,24 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+#Define getter and setter methods for 'isbn' and 'price'
+attr_accessor :isbn, :price
+
+#Constructor that accepts string 'isbn' and float 'price'
+def initialize(isbn, price)
+  # Check if 'isbn' is empty or 'price is less than or equal to 0'
+  if isbn.empty? || price <= 0
+    #Error if the input is invalid
+    raise ArgumentError, 'ISBN can not be empty, and it can not be free!'
+  end
+
+#Set the 'isbn' and 'price' variables
+  @isbn = isbn
+  @price = price 
+end 
+
+#Format the price with a dollar sign, and with 2 decimal places. 
+  def price_as_string
+  "$%.2f" % @price
+  end
+end 
